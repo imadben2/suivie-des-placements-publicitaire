a4bd69962827c1e8565ada215488470bd95d6ec4
imadben2 <imadebensotmi2015@gmail.com>
2021-01-25 21:13:05 +0100

    creaction de fichier migrations

---
+++ app/Http/Controllers/CommerceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Commerce;
+use Illuminate\Http\Request;
+
+class CommerceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Commerce  $commerce
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Commerce $commerce)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Commerce  $commerce
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Commerce $commerce)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Commerce  $commerce
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Commerce $commerce)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Commerce  $commerce
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Commerce $commerce)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_01_22_100242_create_clients_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('agent_de_saisie');
+            $table->integer('N_client');
+            $table->string('Prénom_du_contact');
+            $table->string('Poste_du_contact');
+            $table->string('Raison_sociale');
+            $table->string('Nom_entreprise');
+            $table->string('Entreprise');
+            $table->string('Adresse');
+
+            $table->string('Telephone');
+            $table->string(' e-mail');
+            $table->string('site_internet');
+            $table->string('RC N° :');
+
+            $table->string('NIF');
+            $table->string('NIS');
+            $table->string('secteur_activité');
+            $table->string('nombre_campagne_par_an');
+
+            $table->string('type_de_campagne');
+            $table->string('budget_de_campagne');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
